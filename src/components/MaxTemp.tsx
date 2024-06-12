8d3861eb1432a3487ea276efaa9353e45b63d0be
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-06-12 09:01:46 -0500

    Add resize effect on the nav selector

--- src/components/MaxTemp.tsx
+++ src/components/MaxTemp.tsx
@@ -5,7 +5,7 @@ const koho = KoHo({weight: '300', subsets: ['latin']})
 
 export default function MaxTemp() {
   return (
-    <section className='bg-[#6F0000] h-screen w-full text-[#FFF5F5] flex flex-col items-center justify-center'>
+    <section className='bg-[#6F0000] h-screen w-full text-[#FFF5F5] flex flex-col items-center justify-center' id="max-temp">
       <h1 className={`${koho.className} text-[25vw] tracking-tighter`}>40</h1>
       <div className={styles.celcius} />
       <span className="text-opacity-50 text-white relative bottom-16 text-lg">London</span>
